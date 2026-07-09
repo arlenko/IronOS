de3afd53f48075677858c2d526b31f27cf2e2619
Andrii Pavlenko <andriipawlenko@gmail.com>
2026-07-09 18:28:48 +0300

    Temporary set DC low-voltage threshold to 4V

--- source/Core/Src/Settings.cpp
+++ source/Core/Src/Settings.cpp
@@ -237,7 +237,7 @@ uint8_t lookupVoltageLevel() {
   auto minVoltageOnCell    = getSettingValue(SettingsOptions::MinDCVoltageCells);
   auto minVoltageCellCount = getSettingValue(SettingsOptions::MinVoltageCells);
   if (minVoltageOnCell == 0) {
-    return 90; // 9V since iron does not function effectively below this
+    return 40; // 9V since iron does not function effectively below this
   } else {
     return (minVoltageOnCell * minVoltageCellCount) + (minVoltageCellCount * 2);
   }
